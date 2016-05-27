8aaf45412fbe852e976d3a730fc45a29750214b2
lukimh <lukimh@lukimh-VirtualBox>
2016-05-27 10:13:11 -0300

    Iterface Avaliável pronta

---
+++ src/prog3/trab1/dominio/Avaliável.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog3.trab1.dominio;
+
+/**
+ *
+ * @author lucas
+ */
+public interface Avaliável {
+
+    @Override
+    public String toString();
+}
